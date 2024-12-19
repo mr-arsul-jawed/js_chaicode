e6485ff495030342ceb80f4947055f9296a8f16b
arsul jawed <128362584+mr-arsul-jawed@users.noreply.github.com>
2024-12-19 17:24:23 +0000

    conversion update

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -13,10 +13,10 @@ let score = "arsul"
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 //1 => true;  0 => false
@@ -24,9 +24,64 @@ console.log(booleanIsLoggedIn)
 //"arsul" => true
 
 
-let someNumber = 23
+// let someNumber = 23
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ************************* Operations *************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+//console.log(2 + 2)
+//console.log(2 - 2)
+//console.log(2 * 2)
+//console.log(2 ** 2)
+//console.log(2 / 2)
+//console.log(2 % 2)
+
+let str1 = "hello"
+
+let str2 = " arsul"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+// console.log( "1" + 2);
+// console.log(1 + "3");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "2");
+
+// console.log((3+3) * 5 % 3);
+
+// console.log(+true);
+// console.log(true+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+
+
+
+
+
+
+
+
+
+
+
+
+
 
